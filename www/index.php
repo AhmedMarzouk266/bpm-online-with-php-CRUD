d19c8675d151a216964db2f6523483365221831b
AhmedMarzouk266 <ahmedmarzouk266@gmail.com>
2017-11-28 10:25:19 +0200

    added function to check if the ID exist in the xml file.

--- www/index.php
+++ www/index.php
@@ -6,11 +6,11 @@ ini_set('max_execution_time', 300);
 
 $curl = curl_init();
 $baseUrl =
-'https://005419-marketing.bpmonline.com/0/ServiceModel/EntityDataService.svc/ContactCollection?$top=168';
+'https://014100-sales-enterprise.bpmonline.com/0/ServiceModel/EntityDataService.svc/ContactCollection?select=Id';
 
 
 curl_setopt_array($curl, array(
-    CURLOPT_USERPWD => 'Ahmed Marzouk' . ":" .'Artorg123',
+    CURLOPT_USERPWD => 'Ahmed_Marzouk' . ":" .'Artorg123',
     CURLOPT_RETURNTRANSFER => 1,
     CURLOPT_URL => $baseUrl
 ));
@@ -26,30 +26,35 @@ if($handle = fopen($file,'wt')){
     echo "could not open the file";
 }
 
-$xml = simplexml_load_file($file);
-$result = json_encode($xml);
-
-$jsonFile = 'jsonFile.json';
-if($handle = fopen($jsonFile,'wt')){
-    fwrite($handle,$result);
-    fclose($handle);
-}else{
-    echo "could not open the file";
-}
-
-
-$array = json_decode($result,TRUE);
-
-foreach ($array as $element){
-    echo "<br/><br/>";
-    var_dump($element);
-}
 
 curl_close($curl);
 
-echo gettype($resp)."<br/>";
-var_dump($resp);
+$id='678835c2-6850-49f6-a1ba-33eae62396b1';
+$id_exists = checkId($id);
+if($id_exists){echo "true";}else{echo "false";};
 
+
+//echo gettype($resp)."<br/>";
+//var_dump($resp);
+
+function checkId($id){
+    // this function takes the id and search for the id in the recieved data
+    // if the id exists it return TRUE if not it returns FALSE.
+    if(strlen($id)!= 36){
+        return false;
+    }else{
+        $handle = fopen('testFile.xml', 'r');
+        $valid = false; // init as false
+        while (($buffer = fgets($handle)) !== false) {
+            if (strpos($buffer, $id) !== false) {
+                $valid = TRUE;
+                break; // Once you find the string, you should break out the loop.
+            }
+        }
+        fclose($handle);
+        return $valid;
+    }
+}
 
 
 
